0a14d36e9142b9eeed73fe6666eb09413e61d42d
Viktor <77630291+ViktorKotlov@users.noreply.github.com>
2021-03-15 08:37:13 +0400

    ИТОГ

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var searchBtn = document.querySelector('.button-search')
+var searchForm = document.querySelector('.search-form')
+
+searchBtn.addEventListener('click', function() {
+  searchForm.classList.toggle('search-form--open')
+})
+
+
