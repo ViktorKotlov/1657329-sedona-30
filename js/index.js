584e523e029e5a21f9df9226fea6ebed7fbb40ef
Viktor <77630291+ViktorKotlov@users.noreply.github.com>
2021-03-19 16:51:34 +0400

    добавил анимацию

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var searchBtn = document.querySelector('.button-search')
 var searchForm = document.querySelector('.search-form')
 
 searchBtn.addEventListener('click', function() {
-  searchForm.classList.toggle('search-form--open')
+  searchForm.classList.toggle('search-form--hidden')
 })
 
 
